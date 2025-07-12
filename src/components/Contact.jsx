52f8f135614aee5dfce9352c3376f3434ef7ec31
Balduino Mendes <baldhuino@gmail.com>
2025-07-12 16:33:23 +0100

    Contact data get

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,18 +1,39 @@
 import PageTitle from "./PageTitle";
 import './../assets/styles/order.css'
+import { useState } from "react";
 
 export default function Contact() {
+    const [contact, setContact] = useState({
+        email: '',
+        nome: '',
+        mensagem: ''
+    })
+    function handleChance(e) {
+        const { name, value } = e.target;
+        setContact((prevData) => ({
+            ...prevData,
+            [name]: value
+        }))
+
+    }
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(contact)
+    }
     return (
         <>
             <PageTitle title='Contacte - nos'
                 description='bbbbsd wewesaz mkiiiiiiiiiiiiiik lmkdse' />
-            <div className="controler">
+            <div className="container">
                 <div className="row">
-                    <div className="col-md-6 col-sm-12 col-xs-12">
-                        <form action="">
-                            <input type="email" name="email" id="email" placeholder="Insira eu email" />
-                            <input type="text" name="nome" id="nome" placeholder="Insira seu nome" />
-                            <textarea name="mensagem" id="mensagem" rows='6' placeholder="Insira aqui sua mensagem"></textarea>
+                    <div className="col-md-6 col-sm-12 col-xs-12 form">
+                        <form action="" onSubmit={handleSubmit}>
+                            <input type="email" name="email" id="email" placeholder="Insira eu email"
+                                onChange={handleChance} />
+                            <input type="text" name="nome" id="nome" placeholder="Insira seu nome"
+                                onChange={handleChance} />
+                            <textarea name="mensagem" id="mensagem" rows='6' placeholder="Insira aqui sua mensagem"
+                                onChange={handleChance}></textarea>
                             <input type="submit" name="" id="" value='Enviar' />
                         </form>
                     </div>
